84e99609beebee90fa445fd3ccd5cff3370cdd0c
Pedro Gregório <pedrosgregorio@gmail.com>
2023-10-11 11:59:29 -0300

    Corrigindo Bardo

--- src/main/java/Personagens/Bardo.java
+++ src/main/java/Personagens/Bardo.java
@@ -2,7 +2,6 @@ package Personagens;
 
 import modelos.Arma;
 import modelos.Armadura;
-import modelos.Personagem;
 import modelos.Pocao;
 
 import java.util.List;
@@ -10,15 +9,12 @@ import java.util.List;
 public class Bardo {
 
     private String nome;
-    private int vida;
     private Arma arma;
-    private Armadura armadura;
     private int atordoamento;
 
     private List<Pocao> mochila;
     public Bardo(String nome, Arma instrumento) {
         this.nome = nome;
-        this.vida = 100;
         this.arma = instrumento;
         this.atordoamento = 10;
     }
@@ -42,20 +38,8 @@ public class Bardo {
         return nome;
     }
 
-    public int getVida() {
-        return vida;
-    }
-
-    public void setVida(int vida) {
-        this.vida = vida;
-    }
-
     public Arma getArma() {
         return arma;
-    }
-
-    public Armadura getArmadura() {
-        return armadura;
     }
 
     public void atacar(Boss outro) {
@@ -63,12 +47,5 @@ public class Bardo {
                 this.getNome(), getArma().getNome(), outro.getNome());
         outro.receberDano(getArma().getDano() + atordoamento);
     }
-
-    public void receberDano(int dano) {
-         setVida(getVida() - (dano - getArmadura().getProtecao()) - atordoamento);
-        System.out.printf("%s recebeu dano de %d (Vida: %d)%n",
-                this.getNome(), dano, this.getVida());
-    }
-
 
 }
